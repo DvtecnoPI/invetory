48e4553496e249481b1f2d58ae90c224b4054f56
Diego Veras <verasdiegoca78@gmail.com>
2025-03-17 03:38:07 -0300

    Remove Vite asset references from welcome page to streamline loading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,6 @@
         href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
         rel="stylesheet"
     />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link
         rel="stylesheet"
         href="{{ asset('build/assets/dropzone.css') }}"
