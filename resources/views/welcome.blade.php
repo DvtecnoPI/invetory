543efdb37827753406bb2906edaf55445559c37f
Diego Veras <verasdiegoca78@gmail.com>
2025-03-17 03:51:03 -0300

    Remove unused stylesheet links from welcome page to optimize loading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,25 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Deploy</title>
-        <link
-        rel="stylesheet"
-        href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css"
-    />
-    <!-- Font Awesome para los iconos (opcional) -->
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-    />
 
-    <link rel="preconnect" href="https://fonts.bunny.net" />
-    <link
-        href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
-        rel="stylesheet"
-    />
-    <link
-        rel="stylesheet"
-        href="{{ asset('build/assets/dropzone.css') }}"
-    />
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
