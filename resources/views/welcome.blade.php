d4c64f6f8b7b7be6324a042de30773a748a719f0
Diego Veras <verasdiegoca78@gmail.com>
2025-03-17 04:13:53 -0300

    Add Dropzone CSS stylesheet to welcome page for improved file upload UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://invetory-production.up.railway.app/build/assets/dropzone.css">
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
