ba5bc79137460ba7ac62959e9679d4fe9ebba4dd
Diego Veras <verasdiegoca78@gmail.com>
2025-03-17 03:18:50 -0300

    Update page title from 'DVTecmo' to 'Deploy'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DVTecmo</title>
+        <title>Deploy</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
